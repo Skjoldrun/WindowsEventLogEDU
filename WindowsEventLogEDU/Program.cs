a87dde16d64b22baf7a4429ae998ed6b3dfd55d2
David Halletz <david.halletz@otto-chemie.de>
2023-08-08 14:40:39 +0200

    Initial commmit with project and docu

---
+++ WindowsEventLogEDU/Program.cs
@@ -0,0 +1,13 @@
+﻿using WindowsEventLogEDU.Utilities;
+
+namespace WindowsEventLogEDU
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var someMessage = "This is some message to test the EventLogWriter";
+            EventLogWriter.WriteApplicationEventEntry(someMessage, System.Diagnostics.EventLogEntryType.Error);
+        }
+    }
+}
